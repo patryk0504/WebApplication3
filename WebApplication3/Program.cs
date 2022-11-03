b3961ea07ae01d2a244b60cbf14e1a85ff5ba973
patryk0504 <ppatryk.sledz@gmail.com>
2022-11-03 09:16:09 -0700

    add tests

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -1,5 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 var configuration = (IConfiguration)app.Services.GetService(typeof(IConfiguration))!;
-app.MapGet("/", () => $"Hello World! Value: {configuration.GetSection("test").Value}");
+app.MapGet("/", () => $"FAILING TEST!!!!!! Value: {configuration.GetSection("test").Value}");
 app.Run();
